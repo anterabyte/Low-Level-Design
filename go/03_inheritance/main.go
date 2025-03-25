c4b88595a42c91c48abb7a9ef2454b19a5fa10fa
anterabyte <dragomon27@gmail.com>
2025-03-26 00:06:01 +0530

    Added lld 03_inheritence

--- go/03_inheritance/main.go
+++ go/03_inheritance/main.go
@@ -1,7 +1,29 @@
 package main
 
+import "fmt"
+
+type name struct {
+
+	first string
+	last string
+}
+
+func (n name) eat() {
+	fmt.Println(n.first, n.last,"was eating some food over there" )
+}
+
+type human struct {
+	name
+}
+
+func (h human) flies() {
+	fmt.Println(h.first, h.last,"was flying")
+}
+
 func main() {
 
-	
+	myhuman := human{name{first:"Kal",last:"kothri",}}
 
+	myhuman.eat()
+	myhuman.flies()
 }
